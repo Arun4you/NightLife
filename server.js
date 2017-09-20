0b2ae826ebc9938c121442a862f238c7af74bef2
arun <arun4aue@gmail.com>
2017-09-20 19:01:41 +0530

    added port details

--- server.js
+++ server.js
@@ -1,117 +1,76 @@
 var express = require('express');
-var path = require('path');
-//var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var TwitterStrategy = require('passport-twitter').Strategy;
-var session = require('express-session');
-var passport = require('passport')
-var mainRouter = require('./routes/mainRouter')
-var cors = require('cors');
-var port = process.env.PORT || 3003;
+var path = require('path');
 var app = express();
+var morgan = require('morgan');
+var mongoose = require("mongoose")
+var passport = require('passport')
+var jwt = require('jsonwebtoken')
+var mainRouter = require('./routes/mainRouter')
+var twitterRouter = require('./routes/twitterRouter')
+const TwitterStrategy = require('passport-twitter').Strategy;
+var session = require('express-session');
+var TwitterTokenStrategy = require('passport-twitter-token');
 
-// view engine setup
+app.use(morgan('dev'));
+require('dotenv').config({
+    silent: true
+});
+
+mongoose.connect(process.env.DB, () => {
+    console.log("DB connected");
+})
+
+//app.set('views', path.join(__dirname,'client')); 
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, './views')));
 
-// app.use(session({
-//     secret: 'my_secret',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: true }
-// }))
-app.use(passport.initialize());
-// app.use(passport.session());
-
-
-
-passport.use(new TwitterStrategy({
-        consumerKey: 'Ef8oF6gBVs9zkJ4wGIamY3Jze',
-        consumerSecret: 'gkjv1qPCylTAtonpq07C2PT4YY5vIvAmZe4FaotOyBrqGSMUCB',
-        callbackURL: "https://nightlifebyarun.herokuapp.com/"
-    },
-    function(token, tokenSecret, profile, cb) {
-        User.findOrCreate({ twitterId: profile.id }, function(err, user) {
-            return cb(err, user);
-        });
-    }
-));
-
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(function(req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type, Authorization');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    if (req.method === "OPTIONS") {
-        res.header("application/javascript")
-        res.send(200);
-    } else
-        next();
-}
-
-app.use(allowCrossDomain);
-
-app.get('/express', (req, res) => {
-    res.send("Express")
-});
 
 app.use('/', mainRouter);
+// app.use('/user', twitterRouter);
 
-app.get('/auth/twitter',
+// passport.use(new TwitterStrategy({
+//         consumerKey: process.env.TWITTER_CONSUMER_KEY,
+//         consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+//         callbackUrl: ""
+//     },
+//     function(token, tokenSecret, profile, cb) {
+//         User.findOrCreate({ twitterId: profile.id }, function(err, user) {
+//             return cb(err, user);
+//         });
+//     }
+// ));
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true,
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+app.get('/auth/twitter/',
     passport.authenticate('twitter'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// app.get('/', (req, res) => {
+//         console.log('plesase login')
+//     })
+// router.get('/',
+//     passport.authenticate('twitter', { failureRedirect: '/login' }),
+//     function(req, res) {
+//         // Successful authentication, redirect home.
+//         res.redirect('http://localhost:3000');
+//     });
+
+app.listen(process.env.PORT, function(err) {
+    if (err) throw err;
+    console.log("Server is Running on port " + process.env.PORT);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-app.listen('port', (req, res) => {
-    console.log("Application using " + port);
-})
-
-
-module.exports = app;
